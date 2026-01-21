b1cfc57b0578f25ea90b7e5730669abaa962b81d
unknown <Pawan>
2026-01-21 13:59:34 +1100

    unloading files

---
+++ test.ps1
@@ -0,0 +1,14 @@
+#This is test comment for this file
+
+$var = New-Object System.DateTime
+
+
+<#This is for the 
+multiline comment example#>
+
+# This is to print a string in the console
+Write-Output "Hello World"
+
+#This is to output some String in the console
+
+Write-Output "Today Date and Time is"$var
